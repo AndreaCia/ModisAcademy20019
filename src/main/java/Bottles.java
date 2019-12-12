4405c877492982825f1398cbefbb9b436decfaa2
Andrea Ciaccio <andre.ciaccio@gmail.com>
2019-12-12 17:07:15 +0100

    refator pluralBottles method

--- src/main/java/Bottles.java
+++ src/main/java/Bottles.java
@@ -8,30 +8,34 @@ class Bottles {
     public static void writeLyrics(PrintStream out) {
         for (int beers = 99; beers > -1; beers--) {
             if (beers == 0) {
-                out.print(beers + " bottles" + " of beer on the wall,");
-                out.println(beers + " bottles" + " of beer,");
+                out.print(beers + pluralBottles(beers) + " of beer on the wall,");
+                out.println(beers + pluralBottles(beers) + " of beer,");
                 out.print(takeBeerDown(beers));
                 out.println("99 bottles of beer on the wall.\r\n");
             } else if (beers == 1) {
-                out.print(beers + " bottle" + " of beer on the wall,");
-                out.println(beers + " bottle" + " of beer,");
+                out.print(beers + pluralBottles(beers) + " of beer on the wall,");
+                out.println(beers + pluralBottles(beers) + " of beer,");
                 out.print(takeBeerDown(beers));
-                out.println(beers - 1 + " bottles" + " of beer on the wall.\r\n");
+                out.println(beers - 1 + pluralBottles(beers+1) + " of beer on the wall.\r\n");
             } else if (beers == 2) {
-                out.print(beers + " bottles" + " of beer on the wall,");
-                out.println(beers + " bottles" + " of beer,");
+                out.print(beers + pluralBottles(beers) + " of beer on the wall,");
+                out.println(beers + pluralBottles(beers) + " of beer,");
                 out.print(takeBeerDown(beers));
-                out.println(beers - 1 + " bottle" + " of beer on the wall.\r\n");
+                out.println(beers - 1 + pluralBottles(beers-1) + " of beer on the wall.\r\n");
             } else {
-                out.print(beers + " bottles" + " of beer on the wall,");
-                out.println(beers + " bottles" + " of beer,");
+                out.print(beers + pluralBottles(beers) + " of beer on the wall,");
+                out.println(beers + pluralBottles(beers) + " of beer,");
                 out.print(takeBeerDown(beers));
-                out.println(beers - 1 + " bottles" + " of beer on the wall.\r\n");
+                out.println(beers - 1 + pluralBottles(beers) + " of beer on the wall.\r\n");
             }
         }
     }
 
+    public static String pluralBottles(int beers) {
+        return (beers != 1) ? " bottles" : " bottle";
+    }
+
     public static String takeBeerDown(int beers) {
-        return (beers!=0) ? "Take one down, pass it around," : "Go to the store, buy some more,";
+        return (beers != 0) ? "Take one down, pass it around," : "Go to the store, buy some more,";
     }
 }
