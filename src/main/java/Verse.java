32888cae0009d327b4d3e24583749019e60c7349
Andrea Ciaccio <andre.ciaccio@gmail.com>
2019-12-12 17:56:19 +0100

    create class Verse

---
+++ src/main/java/Verse.java
@@ -0,0 +1,28 @@
+import java.io.PrintStream;
+
+public class Verse {
+    public static void writeLyrics(PrintStream out) {
+        for (int beers = 99; beers > -1; beers--) {
+                out.print(bootles(beers) + " of beer on the wall,");
+                out.println(bootles(beers) + " of beer,");
+                out.print(takeBeerDown(beers));
+                out.println(bootles(beersLeft(beers)) + " of beer on the wall.\r\n");
+        }
+    }
+
+    private static String bootles(int beers) {
+        return beers + pluralInBottle(beers);
+    }
+
+    public static int beersLeft(int beers) {
+        return (beers != 0) ? beers - 1 : 99;
+    }
+
+    public static String pluralInBottle(int beers) {
+        return (beers != 1) ? " bottles" : " bottle";
+    }
+
+    public static String takeBeerDown(int beers) {
+        return (beers != 0) ? "Take one down, pass it around," : "Go to the store, buy some more,";
+    }
+}
