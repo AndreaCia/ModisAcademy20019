1bdbbb4846ab849e99d015921d27351b56e00352
Andrea Ciaccio <andre.ciaccio@gmail.com>
2019-12-12 16:47:37 +0100

    refator takeBeerDown method

--- src/main/java/Bottles.java
+++ src/main/java/Bottles.java
@@ -10,24 +10,28 @@ class Bottles {
             if (beers == 0) {
                 out.print(beers + " bottles" + " of beer on the wall,");
                 out.println(beers + " bottles" + " of beer,");
-                out.print("Go to the store, buy some more,");
+                out.print(takeBeerDown(beers));
                 out.println("99 bottles of beer on the wall.\r\n");
             } else if (beers == 1) {
                 out.print(beers + " bottle" + " of beer on the wall,");
                 out.println(beers + " bottle" + " of beer,");
-                out.print("Take one down, pass it around,");
+                out.print(takeBeerDown(beers));
                 out.println(beers - 1 + " bottles" + " of beer on the wall.\r\n");
             } else if (beers == 2) {
                 out.print(beers + " bottles" + " of beer on the wall,");
                 out.println(beers + " bottles" + " of beer,");
-                out.print("Take one down, pass it around,");
+                out.print(takeBeerDown(beers));
                 out.println(beers - 1 + " bottle" + " of beer on the wall.\r\n");
             } else {
                 out.print(beers + " bottles" + " of beer on the wall,");
                 out.println(beers + " bottles" + " of beer,");
-                out.print("Take one down, pass it around,");
+                out.print(takeBeerDown(beers));
                 out.println(beers - 1 + " bottles" + " of beer on the wall.\r\n");
             }
         }
+    }
+
+    public static String takeBeerDown(int beers) {
+        return (beers!=0) ? "Take one down, pass it around," : "Go to the store, buy some more,";
     }
 }
